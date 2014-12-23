ac541db5109f60c69a384e243df7e6899dfc84c8
Mark Kirschstein <markkirschstein@gmail.com>
2014-12-23 17:27:54 +0000

    button does a pull. it's like magic

--- controllers.js
+++ controllers.js
@@ -5,13 +5,24 @@ boardApp.controller('boardController', function ($scope) {
     {'value': '3',
      'devCost' : '4',     
      'qaCost': '2',
-     'showCard' : function() { return true;}
+     'isReady' : function() { return true;},
+     'pull' : function() { 
+     	this.pull = function() { };
+     	this.isReady = function() { return false;}
+     	$scope.devInProgressTickets.push(this);
+     	var index = $scope.backlogTickets.indexOf(this);
+     	$scope.backlogTickets.splice(index, 1);
+     }
     },    
 
      {'value': '4',
      'devCost' : '6',     
      'qaCost': '1',
-     'showCard' : function() { return false;}
+     'isReady' : function() { return false;}
    },
   ];
+
+   $scope.devInProgressTickets = [];
+
+   
 });
