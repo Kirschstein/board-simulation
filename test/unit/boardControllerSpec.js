32a8775ee5fea9bf6de61aafa1672dbda176064c
unknown <markkirschstein@gmail.com>
2015-01-24 16:16:02 +0000

    reorganising tests for board controller

--- test/unit/boardControllerSpec.js
+++ test/unit/boardControllerSpec.js
@@ -18,34 +18,6 @@ describe('board controllers', function() {
       ctrl = $controller('BoardCtrl', {$scope: scope, Random : random });
     }));
 
-	describe('a BoardCtrl columns', function(){
-
-	    it('includes a backlog queue', function() {
-	    	expect(scope.backlog).toBeDefined();
-	    });
-
-	    it('includes a dev in progress queue', function() {
-	    	expect(scope.devInProgress).toBeDefined();
-	    });
-
-	    it('includes a dev done queue', function() {
-	    	expect(scope.devDone).toBeDefined();
-	    });
-
-	    it('includes a test in progress queue', function() {
-	    	expect(scope.testInProgress).toBeDefined();
-	    });
-
-	    it('includes a test done queue', function() {
-	    	expect(scope.testDone).toBeDefined();
-	    });
-
-	    it('includes a live column', function() {
-	    	expect(scope.live).toBeDefined();
-	    });
-	});
-
-
 	describe('a basic BoardCtrl', function(){
 
 		it('populates the board with a couple of tickets', function(){
---
+++ test/unit/boardControllerSpecs/initialLayoutSpec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+describe('board controllers', function() {
+  	beforeEach(module('boardSimulationApp'));
+
+	var scope, ctrl, $httpBackend, random, workDone;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    	workDone = 2;
+      random = {
+      	nextRandom : function(high,low) {
+      		return workDone;
+      	}
+      };
+
+      $httpBackend = _$httpBackend_;
+      scope = $rootScope.$new();
+      ctrl = $controller('BoardCtrl', {$scope: scope, Random : random });
+    }));
+
+	describe('a BoardCtrl initial layout', function(){
+
+	    it('includes a backlog queue', function() {
+	    	expect(scope.backlog).toBeDefined();
+	    });
+
+		it('has the backlog populated with 4 tickets', function(){
+			expect(scope.backlog.length).toBe(4);
+		});
+
+		it('is ready to pull tickets from backlog', function() {
+			expect(scope.backlog[0].isReady()).toBe(true);
+			expect(scope.backlog[1].isReady()).toBe(true);
+			expect(scope.backlog[2].isReady()).toBe(true);
+		});
+
+	    it('includes a dev in progress queue', function() {
+	    	expect(scope.devInProgress).toBeDefined();
+	    });
+
+	    it('includes a dev done queue', function() {
+	    	expect(scope.devDone).toBeDefined();
+	    });
+
+	    it('includes a test in progress queue', function() {
+	    	expect(scope.testInProgress).toBeDefined();
+	    });
+
+	    it('includes a test done queue', function() {
+	    	expect(scope.testDone).toBeDefined();
+	    });
+
+	    it('includes a live column', function() {
+	    	expect(scope.live).toBeDefined();
+	    });
+
+	});
+
+});
