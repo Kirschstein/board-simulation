425ac04931a9963a5736174166d84fed4a27ca64
Mark Kirschstein <markkirschstein@hotmail.com>
2015-02-01 17:00:28 +0000

    removed phonecat services and reference to controllers

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,6 @@
 
 var boardApp = angular.module('boardSimulationApp', [
   'ngRoute',
-
-  'phonecatControllers',
   'boardControllers',
   'boardServices',
 ]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,17 +1,4 @@
 'use strict';
-
-/* Services */
-
-var phonecatServices = angular.module('phonecatServices', ['ngResource']);
-
-phonecatServices.factory('Phone', ['$resource',
-  function($resource){
-    return $resource('phones/:phoneId.json', {}, {
-      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-    });
-  }]);
-
-
 var boardServices = angular.module('boardServices', []);
 
 boardServices.service('Random', function() {
