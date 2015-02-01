604092d6a6817d6e4f122a7a1e447ecbc676dba5
Mark Kirschstein <markkirschstein@hotmail.com>
2015-02-01 17:01:13 +0000

    removed phonecat controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,26 +1,5 @@
 'use strict';
 
-/* Controllers */
-
-var phonecatControllers = angular.module('phonecatControllers', []);
-
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
-
 var boardControllers = angular.module('boardControllers', []);
 
 boardControllers.controller('BoardCtrl', ['$scope', 'Random', 'BugFactory',
