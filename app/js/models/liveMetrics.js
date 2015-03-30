e87dd9ea8e63898a73e619df9b17a55ddd2599d4
Mark Kirschstein <markkirschstein@hotmail.com>
2015-03-30 13:31:06 +0100

    green -live metrics have separate bugs and tickets count

--- app/js/models/liveMetrics.js
+++ app/js/models/liveMetrics.js
@@ -7,8 +7,16 @@ function LiveMetrics(board) {
 		storiesReleased : 0,
 		bugsReleased : 0,
 		newDay : function() {
+			this.storiesReleased = 0;
+			this.bugsReleased = 0;
 			for (var i=0; i < board.live.length; ++i) {
 				this.cumulativeValue += board.live[i].value;
+				if (board.live[i].type == "story") {
+					this.storiesReleased++;
+				}
+				if(board.live[i].type == "bug") {
+					this.bugsReleased++;
+				}
 			};		
 		}
 	};
--- test/unit/liveMetricsSpecs.js
+++ test/unit/liveMetricsSpecs.js
@@ -3,10 +3,38 @@ describe('live metrics', function() {
 		var board = new Board();
 		var metrics = new LiveMetrics(board);
 
+		var aStory = new Story({
+			value : 1,
+			devCost : 1,
+			qaCost : 1,
+			board : board
+		});
+
+		board.live.push(aStory);
 		metrics.newDay();
 
-		expect(metrics.cumulativeValue).toBe(0);
-		expect(metrics.storiesReleased).toBe(0);
+		expect(metrics.storiesReleased).toBe(1);
 		expect(metrics.bugsReleased).toBe(0);
 	});
+
+	it('keeps a track of released bugs', function(){
+		var board = new Board();
+		var metrics = new LiveMetrics(board);
+
+		var aStory = new Story({
+			value : 1,
+			devCost : 1,
+			qaCost : 1,
+			board : board
+		});
+
+		var aBug = new Bug(1, 1, 1, board, aStory);
+
+		board.live.push(aBug);
+		metrics.newDay();
+
+		expect(metrics.storiesReleased).toBe(0);
+		expect(metrics.bugsReleased).toBe(1);
+	});
+	
 });
